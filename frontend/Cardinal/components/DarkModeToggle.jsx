ccd6d9615ad6fc941f84c01e827f2b29d9b5b3ae
rutamk <rutam2003@gmail.com>
2024-07-08 22:17:37 +0530

    darkmode debug 1

--- frontend/Cardinal/components/DarkModeToggle.jsx
+++ frontend/Cardinal/components/DarkModeToggle.jsx
@@ -1,35 +1,26 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { MdSunny } from 'react-icons/md';
 import { FaMoon } from 'react-icons/fa6';
 
-const DarkModeToggle = () => {
-  const [darkMode, setDarkMode] = useState(() => {
-    // Retrieve the stored dark mode value from localStorage
-    const storedDarkMode = localStorage.getItem('darkMode');
-    return storedDarkMode ? JSON.parse(storedDarkMode) : false;
-  });
-
-  useEffect(() => {
-    // Update the document's class and store the value in localStorage
-    if (darkMode) {
-      document.documentElement.classList.add('dark');
-      localStorage.setItem('darkMode', JSON.stringify(true));
-    } else {
-      document.documentElement.classList.remove('dark');
-      localStorage.setItem('darkMode', JSON.stringify(false));
-    }
-  }, [darkMode]);
-
+const DarkModeToggle = ({ darkMode, setDarkMode }) => {
   const toggleDarkMode = () => {
-    setDarkMode(!darkMode);
+    setDarkMode((prevDarkMode) => !prevDarkMode);
+    const mainContent = document.getElementById('main-content');
+    if (mainContent) {
+      if (darkMode) {
+        mainContent.classList.remove('dark');
+      } else {
+        mainContent.classList.add('dark');
+      }
+    }
   };
 
   return (
     <button
-      className='w-9 h-10 flex items-center justify-center rounded-r-full
-                        text-slate-50 bg-blue-500 hover:bg-blue-600 fixed top-1/2 left-[0px] 
-                          transition-all hover:scale-110 opacity-40 hover:opacity-100 ease-in-out duration-200 
-                        dark:bg-[#ffd84c] dark:text-neutral-950 dark:hover:bg-[#f7c71d]'
+      className={`w-9 h-10 flex items-center justify-center rounded-r-full
+                  text-slate-50 bg-blue-500 hover:bg-blue-600 fixed top-1/2 left-[0px]
+                  transition-all hover:scale-110 opacity-40 hover:opacity-100 ease-in-out duration-200
+                  ${darkMode ? 'dark:bg-[#ffd84c] dark:text-neutral-950 dark:hover:bg-[#f7c71d]' : ''}`}
       onClick={toggleDarkMode}
       aria-label="Toggle Dark Mode"
     >
