6a069f7b66466041d383ed4cc91d8d7eb2e2d832
rutamk <rutam2003@gmail.com>
2024-07-09 04:02:46 +0530

    debug try 2

--- frontend/Cardinal/src/App.jsx
+++ frontend/Cardinal/src/App.jsx
@@ -11,10 +11,10 @@ const routes = (
   <Router>
     <div className="min-h-screen ">
     <Routes>
-      <Route path='/home' exact element={<Home />} />
-      <Route path='/login' exact element={<Login />} />
-      <Route path='/signup' exact element={<SignUp />} />
-      <Route path='/' exact element={<SignUp />} />
+      <Route path='home' exact element={<Home />} />
+      <Route path='login' exact element={<Login />} />
+      <Route path='signup' exact element={<SignUp />} />
+      <Route path='/' exact element={<Login />} />
     </Routes>
     <DarkModeToggle/>
     </div>
