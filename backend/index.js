18b3ab24f96dcba1ba678dff2d6cfa460a11ee75
rutamk <rutam2003@gmail.com>
2024-07-09 15:14:18 +0530

    25

--- backend/index.js
+++ backend/index.js
@@ -14,22 +14,14 @@ const app = express();
 
 const jwt = require("jsonwebtoken");
 const { authenticateToken } = require("./utilities")
-
 // App Use Json
 app.use(express.json());
 
 app.use(
   cors({
-    origin: "*"
-    })
+    origin: "*",
+  })
 );
-
-app.options('*', (req, res) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.sendStatus(204);
-});
 
 app.get("/", (req, res) => {
   res.json({ data: "hello" })
