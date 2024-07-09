6a2e73a2dab3af54ba729a7b08a373b17e5fc15b
rutamk <rutam2003@gmail.com>
2024-07-09 12:45:06 +0530

    18

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "*",
+    origin: '*',
      credentials: true
   })
 );
--- frontend/Cardinal/utils/constants.js
+++ frontend/Cardinal/utils/constants.js
@@ -1,1 +1,1 @@
-export const BASE_URL = 'https://notes-app-backend-beryl.vercel.app/'
+export const BASE_URL = 'https://notes-app-backend-beryl.vercel.app'
