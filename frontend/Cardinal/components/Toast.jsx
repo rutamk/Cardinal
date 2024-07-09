bfb3d69301552e999aa58cade7cba9952e25e811
rutamk <78009334+rutamk@users.noreply.github.com>
2024-07-10 01:54:23 +0530

    Update Toast.jsx

--- frontend/Cardinal/components/Toast.jsx
+++ frontend/Cardinal/components/Toast.jsx
@@ -18,7 +18,7 @@ const Toast = ({ isShown, message, type, onClose }) => {
     return (
         <div
   className={`fixed bottom-10 left-6 transition-opacity ease-in-out ${
-    isShown ? "opacity-100" : "opacity-0 w-0 h-0"
+    isShown ? "opacity-100 z-10" : "opacity-0 w-0 h-0"
   } duration-500`}
 >
   <div
